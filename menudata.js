4cbb90feef3067280892ac3c07ed5a538489a64f
笑到猝死的家伙 <112929698+Laihb1106205841@users.noreply.github.com>
2024-05-20 01:37:16 +0800

    Add files via upload

--- menudata.js
+++ menudata.js
@@ -24,6 +24,10 @@
 */
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
+{text:"Topics",url:"topics.html"},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
-{text:"Class Index",url:"classes.html"}]}]}
+{text:"Class Index",url:"classes.html"},
+{text:"Class Members",url:"functions.html",children:[
+{text:"All",url:"functions.html"},
+{text:"Functions",url:"functions_func.html"}]}]}]}
